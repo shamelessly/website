7cbe0df2c57c9eef12de7e3025e4f9bccc3f2808
Jean-Baptiste PIN <jean-baptiste.pin@keatonrow.com>
2014-02-09 15:22:45 -0500

    remove cache

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,10 @@
 var shame = require('../models/shame');
 
 function getTweet(req, res, next){
+  // Disable caching for content files
+  res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+  res.header("Pragma", "no-cache");
+  res.header("Expires", 0);
 	shame.getTweet(function(err, result){
 		res.json(result);
 	});
