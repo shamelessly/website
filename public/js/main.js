01c2cd848862720b3085bef7e4421ff501a36b57
Razvan L-M <razvan.lada@gmail.com>
2014-02-08 21:59:56 -0500

    added frame 100%

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,11 @@
 
+$(document).ready(function(){
+	resize();
+});
+
+$(document).on('resize', resize)
+
+function resize(){
+	var height = $(window).height();
+	$('.row-top').css({height:height});
+}
